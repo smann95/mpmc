adb0c4a16b7c632abc9d85285f5c49935469089f
L <luciano.e.laratelli@gmail.com>
2018-04-06 11:25:13 -0400

    blah

--- run_tests.sh
+++ run_tests.sh
@@ -3,12 +3,11 @@
 newest_commit=$(git rev-parse HEAD)
 
 
-for commit in $(git rev-list master)
+for commit in $(git rev-list master | head)
 do
   git checkout -b "${commit}" ${commit}
   bash compile.sh
-  exit
 done
 
-#git checkout master
-#git branch | grep -v "master" | grep -v "mpmc_tester" | xargs git branch -D
+git checkout test_suite
+git branch | grep -v "master" | grep -v "test_suite" | xargs git branch -D
